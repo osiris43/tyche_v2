a4d466b4e6ecc25c95da50bf0bd0bebc9aaf7043
osiris43 <osiris43@gmail.com>
2025-02-23 08:54:01 -0600

    maybe deploy from gha

--- app/main/models.py
+++ app/main/models.py
@@ -1,6 +1,8 @@
 from flask import current_app
 
 
+# testing something
 def get_dynamodb_table():
+    print("hello")
     """Retrieve the DynamoDB table object"""
     return current_app.dynamodb.Table("tyche")
